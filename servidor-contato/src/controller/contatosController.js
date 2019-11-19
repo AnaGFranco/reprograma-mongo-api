c004c268a5dcdd7f638c5426f21e440d838b8b9c
AnaGFranco <anagsfranco@hotmail.com>
2019-11-18 22:03:45 -0300

    Aplicando o mongoDB na api

--- servidor-contato/src/controller/contatosController.js
+++ servidor-contato/src/controller/contatosController.js
@@ -1,13 +1,36 @@
-const model = require("../model/contatos")
+const contatosCollection = require("../model/contatoSchema")
 
 const getAll = (request, response) => {
-  console.log(request.url)
-  response.status(200).send(model.agenda)
+  // console.log(request.url)
+
+  contatosCollection.find((error,contatos) => {
+    if(error){
+      return response.status(500).send(error)
+    }else{
+    return response.status(200).send(contatos)
+    }
+  })
+
+  // response.status(200).send(model.agenda)
 };
 
 const add = (request, response) => {
-  let contato = request.body
-  let baseDados = model.agenda.contatos
+// novo objeto para a nossa coleção
+const contatoDoBody = request.body
+const contato = new contatosCollection(contatoDoBody)
+
+  contato.save((error) =>{
+  // if(error !== null && error !== undefined)
+    if(error){
+      return response.status(400).send(error)
+    }else{
+    return response.status(201).send(contatos)
+    }
+  })
+}
+
+  /* let contato = request.body
+  let baseDados = contatoCollection.agenda.contatos
   contato.id = Math.random().toString(36).substr(-8)
 
   if (!contato.nome || !contato.dataNascimento || !contato.celular) {
@@ -16,12 +39,10 @@ const add = (request, response) => {
     if (baseDados.find(dado => dado.nome === contato.nome)) {
       response.status(400).send("Contato já cadastrado")
     } else {
-      model.agenda.contatos.push(contato)
+      contatoCollection.agenda.contatos.push(contato)
       response.status(201).send(contato)
     }
-  }
-
-}
+  } */
 
 module.exports = {
   getAll,
